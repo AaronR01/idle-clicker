3fd43b78bfe2026246f0c8cdcaf3c1fdb1a9eb4b
Aaron <aaron@ice.ufjf.br>
2023-10-24 20:51:55 -0300

    Boa parte do app do mini curso encurtador.com.br/aeAc7

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
+import Cabecalho from './components/Cabecalho/Cabecalho.js';
 import './App.css';
+import Recursos from './components/Recursos/Recursos.js';
+import Acoes from './components/Acoes/Acoes.js';
+import React, {useState} from 'react';
 
-function App() {
+
+export default function App() {
+  const [estoque, setEstoque] = useState({
+    madeira: 0,
+    ouro: 0
+  });
+  
+  const handleClick = (e) => {
+    console.log(e.target.id);
+  };
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Cabecalho />
+      <Recursos estoque={estoque} />
+      <Acoes handleClick={handleClick} />
+      
     </div>
   );
 }
-
-export default App;
